6c30db52b2d342a67cfcef8c7cc62fa3806de0b8
Azat Mukhametzyanov <amukhametzyanov@fix.ru>
2019-09-10 12:30:08 +0300

    add java doc

--- distributed-job-manager/src/main/java/ru/fix/distributed/job/manager/model/ZookeeperState.java
+++ distributed-job-manager/src/main/java/ru/fix/distributed/job/manager/model/ZookeeperState.java
@@ -8,16 +8,34 @@ import java.util.*;
  */
 public class ZookeeperState extends HashMap<WorkerItem, List<WorkItem>> {
 
+    /**
+     * If worker exists, add new work item to work item's list,
+     * else create worker and add new work item
+     */
     public void addWorkItem(WorkerItem worker, WorkItem workItem) {
         if (this.containsKey(worker)) {
             List<WorkItem> workItems = new ArrayList<>(this.get(worker));
             workItems.add(workItem);
             this.put(worker, workItems);
         } else {
-            this.put(worker, Arrays.asList(workItem));
+            this.put(worker, Collections.singletonList(workItem));
         }
     }
 
+
+    public void addWorkItems(WorkerItem worker, List<WorkItem> workItems) {
+        if (this.containsKey(worker)) {
+            List<WorkItem> newWorkItems = new ArrayList<>(this.get(worker));
+            newWorkItems.addAll(workItems);
+            this.put(worker, newWorkItems);
+        } else {
+            this.put(worker, workItems);
+        }
+    }
+
+    /**
+     * @return worker which has less work pool size (doesn't depends on job)
+     */
     public WorkerItem getLessBusyWorker() {
         WorkerItem lessBusyWorker = null;
         int minWorkPool = Integer.MAX_VALUE;
@@ -48,6 +66,36 @@ public class ZookeeperState extends HashMap<WorkerItem, List<WorkItem>> {
         return this;
     }
 
+    /**
+     * @return true, if work pool sizes of various workers differ more than 1
+     * For example:
+     * worker-1: 4
+     * worker-2: 5
+     * worker-3: 5
+     * is balanced, returns true
+     * worker-1: 0
+     * worker-2: 0
+     * returns also true
+     * worker-1: 4
+     * worker-2: 5
+     * worker-3: 3
+     * returns false
+     */
+    public boolean isBalanced() {
+        for (Map.Entry<WorkerItem, List<WorkItem>> worker : entrySet()) {
+            int workPoolSize = worker.getValue().size();
+
+            for (Map.Entry<WorkerItem, List<WorkItem>> worker1 : entrySet()) {
+                int workPoolSize1 = worker1.getValue().size();
+
+                if (workPoolSize - workPoolSize1 > 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
     @Override
     public String toString() {
         StringBuilder result = new StringBuilder("Zookeeper state\n");
@@ -59,11 +107,10 @@ public class ZookeeperState extends HashMap<WorkerItem, List<WorkItem>> {
             result.append("\t└ ").append(workerId).append("\n");
 
             for (WorkItem workItem : workItems) {
-                result.append("\t\t└ ").append(workItem.getJobId()).append(" - ").append(workItem.getId()).append("\n");
+                result.append("\t\t└ ").append(workItem.getJobId()).append(" - ")
+                        .append(workItem.getId()).append("\n");
             }
-
         }
-
         return result.toString();
     }
 
---
+++ distributed-job-manager/src/test/java/ru/fix/distributed/job/manager/strategy/EvenlySpreadAssignmentStrategyTest.java
@@ -0,0 +1,12 @@
+package ru.fix.distributed.job.manager.strategy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EvenlySpreadAssignmentStrategyTest {
+
+    @Test
+    void reassignAndBalance() {
+    }
+}
