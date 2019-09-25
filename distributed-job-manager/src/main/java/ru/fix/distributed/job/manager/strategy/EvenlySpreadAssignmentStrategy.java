60235323251285b3794eae90f41f738f2ab4df04
Azat Mukhametzyanov <amukhametzyanov@fix.ru>
2019-09-25 15:12:13 +0300

    cleanup

--- distributed-job-manager/src/main/java/ru/fix/distributed/job/manager/strategy/EvenlySpreadAssignmentStrategy.java
+++ distributed-job-manager/src/main/java/ru/fix/distributed/job/manager/strategy/EvenlySpreadAssignmentStrategy.java
@@ -11,46 +11,6 @@ import java.util.Set;
 
 public class EvenlySpreadAssignmentStrategy extends AbstractAssignmentStrategy {
 
-    public AssignmentState reassignAndBalance(
-            Map<JobId, AssignmentState> availability,
-            AssignmentState prevAssignment,
-            AssignmentState currentAssignment
-    ) {
-
-        for (AssignmentState availabilityState : availability.values()) {
-            HashSet<WorkItem> itemsToAssign = new HashSet<>();
-            availabilityState.values().forEach(itemsToAssign::addAll);
-
-            int workersCount = availabilityState.size();
-            int workItemsCount = itemsToAssign.size();
-            int limit = workItemsCount / workersCount;
-            int itemsAssignedFromPreviousCounter = 0;
-
-            for (Map.Entry<WorkerId, HashSet<WorkItem>> workerEntry : availabilityState.entrySet()) {
-                currentAssignment.putIfAbsent(workerEntry.getKey(), new HashSet<>());
-            }
-
-            for (WorkItem item : itemsToAssign) {
-                WorkerId workerFromPrevious = prevAssignment.getWorkerOfWorkItem(item);
-
-                if (prevAssignment.containsWorkItem(item)
-                        && itemsAssignedFromPreviousCounter < limit
-                        && currentAssignment.containsKey(workerFromPrevious)) {
-
-                    currentAssignment.addWorkItem(workerFromPrevious, item);
-                    itemsAssignedFromPreviousCounter++;
-                } else {
-                    WorkerId lessBusyWorker = currentAssignment
-                            .getLessBusyWorkerFromAvailableWorkers(availabilityState.keySet());
-                    currentAssignment.addWorkItem(lessBusyWorker, item);
-                }
-            }
-
-        }
-
-        return currentAssignment;
-    }
-
     @Override
     public AssignmentState reassignAndBalance(
             Map<JobId, Set<WorkerId>> availability,
--- distributed-job-manager/src/main/java/ru/fix/distributed/job/manager/strategy/RendezvousHashAssignmentStrategy.java
+++ distributed-job-manager/src/main/java/ru/fix/distributed/job/manager/strategy/RendezvousHashAssignmentStrategy.java
@@ -19,43 +19,6 @@ import java.util.Set;
 public class RendezvousHashAssignmentStrategy extends AbstractAssignmentStrategy {
     private static final Logger log = LoggerFactory.getLogger(RendezvousHashAssignmentStrategy.class);
 
-    public AssignmentState reassignAndBalance(
-            Map<JobId, AssignmentState> availability,
-            AssignmentState prevAssignment,
-            AssignmentState currentAssignment
-    ) {
-        final Funnel<String> stringFunnel = (from, into) -> {
-            try {
-                into.putBytes(from.getBytes("UTF-8"));
-            } catch (UnsupportedEncodingException e) {
-                log.warn("Can't reassign and balance: unsupported encoding", e);
-            }
-        };
-
-
-        for (Map.Entry<JobId, AssignmentState> jobEntry : availability.entrySet()) {
-            final RendezvousHash<String, String> hash = new RendezvousHash<>(
-                    Hashing.murmur3_128(), stringFunnel, stringFunnel, new ArrayList<>()
-            );
-            jobEntry.getValue().keySet().forEach(worker -> hash.add(worker.getId()));
-
-            for (Map.Entry<WorkerId, HashSet<WorkItem>> workerEntry : jobEntry.getValue().entrySet()) {
-                currentAssignment.putIfAbsent(workerEntry.getKey(), new HashSet<>());
-
-                for (WorkItem workItem : workerEntry.getValue()) {
-                    if (currentAssignment.containsWorkItem(workItem)) {
-                        continue;
-                    }
-
-                    String workerId = hash.get(workItem.getJobId() + "_" + workItem.getId());
-                    currentAssignment.addWorkItem(new WorkerId(workerId), workItem);
-                }
-            }
-        }
-
-        return currentAssignment;
-    }
-
     @Override
     public AssignmentState reassignAndBalance(
             Map<JobId, Set<WorkerId>> availability,
