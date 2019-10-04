7cb02ab676fd83d7e7b2a95bc813196bdf42f341
Azat Mukhametzyanov <amukhametzyanov@fix.ru>
2019-10-04 13:05:59 +0300

    refactor

--- distributed-job-manager/src/main/java/ru/fix/distributed/job/manager/strategy/EvenlySpreadAssignmentStrategy.java
+++ distributed-job-manager/src/main/java/ru/fix/distributed/job/manager/strategy/EvenlySpreadAssignmentStrategy.java
@@ -21,37 +21,30 @@ public class EvenlySpreadAssignmentStrategy extends AbstractAssignmentStrategy {
         for (Map.Entry<JobId, Set<WorkerId>> jobEntry : availability.entrySet()) {
             Set<WorkItem> itemsToAssignForJob = getWorkItemsByJob(jobEntry.getKey(), itemsToAssign);
             jobEntry.getValue().forEach(e -> currentAssignment.putIfAbsent(e, new HashSet<>()));
-            
+
             int workersCount = jobEntry.getValue().size();
             int workItemsCount = itemsToAssignForJob.size();
-            int countWorkersContainingMaxPools = workItemsCount % workersCount;
             int limitWorkItemsOnWorker = limitWorkItemsOnWorker(workItemsCount, workersCount);
             int itemsAssignedFromPreviousCounter = 0;
-            int workersReachedLimitCounter = 0;
 
             for (WorkItem item : itemsToAssignForJob) {
                 WorkerId workerFromPrevious = prevAssignment.getWorkerOfWorkItem(item);
 
                 if (prevAssignment.containsWorkItem(item)
+                        && currentAssignment.containsKey(workerFromPrevious)
+                        && currentAssignment.isBalanced()
+                        && currentAssignment.isBalancedByJobId(item.getJobId())
                         && itemsAssignedFromPreviousCounter < limitWorkItemsOnWorker
-                        && (workersReachedLimitCounter != countWorkersContainingMaxPools || countWorkersContainingMaxPools == 0)
-                        && currentAssignment.containsKey(workerFromPrevious)) {
-
+                ) {
                     currentAssignment.addWorkItem(workerFromPrevious, item);
                     itemsAssignedFromPreviousCounter++;
 
-                    int itemsAssignedOnWorker = currentAssignment.get(workerFromPrevious, item.getJobId()).size();
-                    if (itemsAssignedOnWorker == limitWorkItemsOnWorker) {
-                        workersReachedLimitCounter++;
-                    }
-
                     int limitItemsOnWorkerInPreviousState = prevAssignment
                             .get(workerFromPrevious, jobEntry.getKey()).size();
-                    if (limitItemsOnWorkerInPreviousState < itemsAssignedFromPreviousCounter) {
+                    if (limitItemsOnWorkerInPreviousState == itemsAssignedFromPreviousCounter) {
                         itemsAssignedFromPreviousCounter = 0;
                     }
                 } else {
-                    itemsAssignedFromPreviousCounter = 0;
                     WorkerId lessBusyWorker = currentAssignment
                             .getLessBusyWorkerFromAvailableWorkers(jobEntry.getValue());
                     currentAssignment.addWorkItem(lessBusyWorker, item);
