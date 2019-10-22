2d1e5a667e5a33d0dc41270825c510837737204c
Azat Mukhametzyanov <amukhametzyanov@fix.ru>
2019-10-22 11:53:21 +0300

    try to fix untable test

--- distributed-job-manager/src/test/kotlin/ru/fix/distributed/job/manager/DistributedJobManagerTest.kt
+++ distributed-job-manager/src/test/kotlin/ru/fix/distributed/job/manager/DistributedJobManagerTest.kt
@@ -187,7 +187,7 @@ internal class DistributedJobManagerTest : AbstractJobManagerTest() {
                 .atMost(Duration.ofSeconds(3))
                 .pollDelay(Duration.ofMillis(200))
                 .until { !workersAlive("worker-2") }
-        Thread.sleep(400)
+        Thread.sleep(2000)
 
         assignedState = readAssignedState(curatorFramework)
         assertTrue(assignedState.isBalanced)
