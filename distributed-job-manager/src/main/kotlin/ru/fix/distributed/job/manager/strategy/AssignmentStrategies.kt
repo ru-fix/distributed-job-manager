39357e80f25a4b026723a229c9f7d9741006ccd1
Azat Mukhametzyanov <amukhametzyanov@fix.ru>
2019-10-30 16:55:01 +0300

    change default strategy to evenly rendezvous

--- distributed-job-manager/src/main/kotlin/ru/fix/distributed/job/manager/strategy/AssignmentStrategies.kt
+++ distributed-job-manager/src/main/kotlin/ru/fix/distributed/job/manager/strategy/AssignmentStrategies.kt
@@ -4,7 +4,7 @@ class AssignmentStrategies {
     companion object {
         val EVENLY_SPREAD: AssignmentStrategy = EvenlySpreadAssignmentStrategy()
         val RENDEZVOUS: AssignmentStrategy = RendezvousHashAssignmentStrategy()
-        val DEFAULT: AssignmentStrategy = EvenlySpreadAssignmentStrategy()
+        val DEFAULT: AssignmentStrategy = EvenlyRendezvousAssignmentStrategy()
         val EVENLY_RENDEZVOUS: AssignmentStrategy = EvenlyRendezvousAssignmentStrategy()
     }
 }
