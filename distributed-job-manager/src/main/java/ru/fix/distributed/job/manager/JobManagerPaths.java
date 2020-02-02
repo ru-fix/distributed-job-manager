efd2c4a25fe984d2034bf70188f1f5563b909c6f
kazankovMarch <kazankov.march@gmail.com>
2020-02-02 15:35:54 +0300

    issue-22 replace method invocations in JobManagerPaths with full ZK paths, replace raw strings with constants

--- distributed-job-manager/src/main/java/ru/fix/distributed/job/manager/JobManagerPaths.java
+++ distributed-job-manager/src/main/java/ru/fix/distributed/job/manager/JobManagerPaths.java
@@ -3,10 +3,16 @@ package ru.fix.distributed.job.manager;
 import org.apache.curator.utils.ZKPaths;
 
 class JobManagerPaths {
+    public static final String ALIVE = "alive";
+    public static final String ASSIGNMENT_VERSION = "assignment-version";
+    public static final String LEADER_LATCH = "leader-latch";
+    public static final String LOCKS = "locks";
+    public static final String REGISTRATION_VERSION = "registration-version";
+    public static final String WORKERS = "workers";
+    public static final String ASSIGNED = "assigned";
+    public static final String AVAILABLE = "available";
     public static final String WORK_POOLED_JOB_ID = "work-pooled";
     public static final String WORK_POOL = "work-pool";
-    public static final String REGISTRATION_VERSION = "registration-version";
-    public static final String ASSIGNMENT_VERSION = "assignment-version";
 
     final String rootPath;
 
@@ -14,8 +20,29 @@ class JobManagerPaths {
         this.rootPath = rootPath;
     }
 
+    public String getWorkersAlivePath() {
+        return ZKPaths.makePath(rootPath, ALIVE);
+    }
+
+    String getWorkerAliveFlagPath(String workerId) {
+        return ZKPaths.makePath(rootPath, ALIVE, workerId);
+    }
+
     String getAssignmentVersion() {
         return ZKPaths.makePath(rootPath, ASSIGNMENT_VERSION);
+    }
+
+    String getLeaderLatchPath() {
+        return ZKPaths.makePath(rootPath, LEADER_LATCH);
+    }
+
+    String getWorkPooledLocksPath() {
+        return ZKPaths.makePath(rootPath, LOCKS, WORK_POOLED_JOB_ID);
+    }
+
+    String getWorkItemLock(String jobId, String workItem) {
+        return ZKPaths.makePath(rootPath, LOCKS, WORK_POOLED_JOB_ID, jobId,
+                String.format("work-share-%s.lock", workItem));
     }
 
     String getRegistrationVersion() {
@@ -23,68 +50,47 @@ class JobManagerPaths {
     }
 
     String getWorkersPath() {
-        return ZKPaths.makePath(rootPath, "workers");
-    }
-
-    String getWorkerAliveFlagPath(String workerId) {
-        return ZKPaths.makePath(getWorkersAlivePath(), workerId);
+        return ZKPaths.makePath(rootPath, WORKERS);
     }
 
     String getWorkerPath(String workerId) {
-        return ZKPaths.makePath(getWorkersPath(), workerId);
+        return ZKPaths.makePath(rootPath, WORKERS, workerId);
     }
 
-    String getWorkerAvailableJobsPath(String worker) {
-        return ZKPaths.makePath(getWorkerPath(worker), "available");
-    }
-
-    String getWorkerAssignedJobsPath(String worker) {
-        return ZKPaths.makePath(getWorkerPath(worker), "assigned");
-    }
-
-    String getLeaderLatchPath() {
-        return ZKPaths.makePath(rootPath, "leader-latch");
-    }
-
-    public String getWorkersAlivePath() {
-        return ZKPaths.makePath(rootPath, "alive");
-    }
-
-    String getAvailableWorkPooledJobPath(String workerId) {
-        return ZKPaths.makePath(getWorkerPath(workerId), "available", WORK_POOLED_JOB_ID);
-    }
-
-    String getAvailableWorkPoolPath(String workerId, String jobId) {
-        return ZKPaths.makePath(getAvailableWorkPooledJobPath(workerId), jobId, WORK_POOL);
+    String getWorkerAssignedJobsPath(String workerId) {
+        return ZKPaths.makePath(rootPath, WORKERS, workerId, ASSIGNED);
     }
 
     String getAssignedWorkPoolPath(String workerId, String jobId) {
-        return ZKPaths.makePath(getAssignedWorkPooledJobsPath(workerId), jobId, WORK_POOL);
+        return ZKPaths.makePath(rootPath, WORKERS, workerId, ASSIGNED, WORK_POOLED_JOB_ID, jobId, WORK_POOL);
     }
 
     String getAssignedWorkPooledJobsPath(String workerId) {
-        return ZKPaths.makePath(getWorkersPath(), workerId, "assigned", WORK_POOLED_JOB_ID);
+        return ZKPaths.makePath(rootPath, WORKERS, workerId, ASSIGNED, WORK_POOLED_JOB_ID);
     }
 
     String getAssignedWorkPooledJobsPath(String workerId, String jobId) {
-        return ZKPaths.makePath(getAssignedWorkPooledJobsPath(workerId), jobId);
-    }
-
-    String getAvailableWorkItemPath(String workerId, String jobId, String workItemId) {
-        return ZKPaths.makePath(getAvailableWorkPoolPath(workerId, jobId), workItemId);
+        return ZKPaths.makePath(rootPath, WORKERS, workerId, ASSIGNED, WORK_POOLED_JOB_ID, jobId);
     }
 
     String getAssignedWorkItemPath(String workerId, String jobId, String workItemId) {
-        return ZKPaths.makePath(getAssignedWorkPoolPath(workerId, jobId), workItemId);
+        return ZKPaths.makePath(rootPath, WORKERS, workerId, ASSIGNED, WORK_POOLED_JOB_ID, jobId, WORK_POOL, workItemId);
     }
 
-    String getWorkPooledLocksPath() {
-        return ZKPaths.makePath(rootPath, "locks", WORK_POOLED_JOB_ID);
+    String getWorkerAvailableJobsPath(String workerId) {
+        return ZKPaths.makePath(rootPath, WORKERS, workerId, AVAILABLE);
     }
 
-    String getWorkItemLock(String jobId, String workItem) {
-        return ZKPaths.makePath(rootPath, "locks", WORK_POOLED_JOB_ID, jobId,
-                String.format("work-share-%s.lock", workItem));
+    String getAvailableWorkPooledJobPath(String workerId) {
+        return ZKPaths.makePath(rootPath, WORKERS, workerId, AVAILABLE, WORK_POOLED_JOB_ID);
+    }
+
+    String getAvailableWorkPoolPath(String workerId, String jobId) {
+        return ZKPaths.makePath(rootPath, WORKERS, workerId, AVAILABLE, WORK_POOLED_JOB_ID, jobId, WORK_POOL);
+    }
+
+    String getAvailableWorkItemPath(String workerId, String jobId, String workItemId) {
+        return ZKPaths.makePath(rootPath, WORKERS, workerId, AVAILABLE, WORK_POOLED_JOB_ID, jobId, WORK_POOL, workItemId);
     }
 
 }
