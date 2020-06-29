256ba081a7c5a99666e14df0bb243dab44cad3d7
kasfandiyarov <VkyabOvtTzp1821>
2020-06-29 11:18:02 +0300

    Assignment strategies acceptance tests

--- distributed-job-manager/src/test/kotlin/ru/fix/distributed/job/manager/strategy/AssignmentStrategyAcceptanceTest.kt
+++ distributed-job-manager/src/test/kotlin/ru/fix/distributed/job/manager/strategy/AssignmentStrategyAcceptanceTest.kt
@@ -9,11 +9,12 @@ import org.junit.jupiter.params.provider.MethodSource
 import ru.fix.distributed.job.manager.model.AssignmentState
 import ru.fix.distributed.job.manager.model.JobId
 import ru.fix.distributed.job.manager.model.WorkItem
+import ru.fix.distributed.job.manager.model.WorkerId
 import java.util.stream.Stream
 
 class AssignmentStrategyAcceptanceTest {
 
-    companion object{
+    companion object {
         @JvmStatic
         fun strategies(): Stream<Arguments> {
             return Stream.of(
@@ -65,11 +66,11 @@ class AssignmentStrategyAcceptanceTest {
 
     @ParameterizedTest
     @MethodSource("strategies")
-    fun `one WorkItem can be assigned only once to particular worker`() {
+    fun `one WorkItem can be assigned only once to particular worker`(
+            strategy: AssignmentStrategy
+    ) {
         val available = availability {
             "job-A"("worker-0", "worker-1", "worker-2")
-            "job-B"("worker-0", "worker-1", "worker-2")
-            "job-C"("worker-0", "worker-1", "worker-2")
         }
         val previous = assignmentState {
             "worker-0"{}
@@ -77,26 +78,17 @@ class AssignmentStrategyAcceptanceTest {
             "worker-2"{}
         }
         val workItems = workItems {
-            "job-A"((1..10))
-            "job-B"("B0")
-            "job-C"("C0")
-        }
-
-        val expected = assignmentState {
-            "worker-0"{
-                "job-A"("A0")
-            }
-            "worker-1"{
-                "job-B"("B0")
-            }
-            "worker-2"{
-                "job-C"("C0")
-            }
+            "job-A"(*(1..10).map { "A$it" }.toTypedArray())
         }
 
         val current = AssignmentState()
         strategy.reassignAndBalance(available, previous, current, workItems)
-        Assertions.assertEquals(expected, current, "strategy: ${strategy.javaClass}")
 
+        for (workItem in workItems) {
+            Assertions.assertEquals(1, current.values.flatMap { it }.count(),
+                    "WorkItem $workItem assignment once")
+
+            current[WorkerId("worker-0")]
+        }
     }
 }
