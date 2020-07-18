5df11396d8dd6f9b050493f6e283ea6def93bd57
akazankov <akazankov@fix.ru>
2020-07-18 13:42:16 +0300

    move resolveJobId logic from AnnotationResolver object to JobDescriptor.kt

--- distributed-job-manager/src/main/kotlin/ru/fix/distributed/job/manager/model/JobDescriptor.kt
+++ distributed-job-manager/src/main/kotlin/ru/fix/distributed/job/manager/model/JobDescriptor.kt
@@ -1,12 +1,16 @@
 package ru.fix.distributed.job.manager.model
 
-import ru.fix.distributed.job.manager.*
+import ru.fix.distributed.job.manager.DistributedJob
+import ru.fix.distributed.job.manager.DistributedJobContext
+import ru.fix.distributed.job.manager.WorkPool
+import ru.fix.distributed.job.manager.WorkPoolRunningStrategy
+import ru.fix.distributed.job.manager.annotation.JobIdField
 import ru.fix.dynamic.property.api.DynamicProperty
 import ru.fix.stdlib.concurrency.threads.Schedule
 
 class JobDescriptor(private val job: DistributedJob) {
 
-    private val jobId: String = job.getJobId() ?: AnnotationResolver.resolveJobId(job)
+    private val jobId: String = resolveJobId(job)
 
     fun getJobId(): String = jobId
 
@@ -21,4 +25,23 @@ class JobDescriptor(private val job: DistributedJob) {
     fun getWorkPool(): WorkPool = job.getWorkPool()
 
     fun getWorkPoolCheckPeriod(): Long = job.getWorkPoolCheckPeriod()
+}
+
+fun resolveJobId(job: DistributedJob): String {
+    val jobIdByGetter = job.getJobId()
+    if (jobIdByGetter != null) {
+        return jobIdByGetter
+    }
+    for (field in job.javaClass.declaredFields) {
+        if (field.isAnnotationPresent(JobIdField::class.java)) {
+            try {
+                field.isAccessible = true
+                return field[job] as String
+            } catch (e: Exception) {
+                throw IllegalStateException(
+                        "Some troubles with getting jobId by annotation. Maybe it's not a String field?", e)
+            }
+        }
+    }
+    return job.javaClass.name
 }
