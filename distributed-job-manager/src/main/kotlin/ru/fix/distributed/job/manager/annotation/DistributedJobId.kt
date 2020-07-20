9e360f042d8f7ce233030fd32934a5bf7b076009
akazankov <akazankov@fix.ru>
2020-07-20 17:45:45 +0300

    update javadoc for DistributedJobId annotation

--- distributed-job-manager/src/main/kotlin/ru/fix/distributed/job/manager/annotation/DistributedJobId.kt
+++ distributed-job-manager/src/main/kotlin/ru/fix/distributed/job/manager/annotation/DistributedJobId.kt
@@ -1,10 +1,19 @@
 package ru.fix.distributed.job.manager.annotation
 
+import ru.fix.distributed.job.manager.DistributedJob
+
 /**
- * For String field in DistributedJobs, which represents id of that job
- * If field with that annotation won't be found, for job id will be used full class name
+ * For declaring identifier of job by annotation:
+ * ```
+ * @DistributedJobId("some-job")
+ * class SomeJob : DistributedJob {
+ * //...
+ * }
+ * ```
+ * This annotation will be ignored, if you override [DistributedJob.jobId].
  *
- * Now you can change that behaviour by overriding {@link ru.fix.distributed.job.manager.DistributedJob}
+ * If you don't use neither this annotation neither [DistributedJob.jobId],
+ * full class name of job will be used as jobId
  * */
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
